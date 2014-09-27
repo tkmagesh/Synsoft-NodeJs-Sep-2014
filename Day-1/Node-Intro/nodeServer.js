d0225f333f38b4c37f9431467102cbb9c95b031e
tkmagesh <tkmagesh77@gmail.com>
2014-09-27 16:23:20 +0530

    fixing the fs.stat bug

--- Day-1/Node-Intro/nodeServer.js
+++ Day-1/Node-Intro/nodeServer.js
@@ -3,34 +3,39 @@ var http = require('http'),
     url = require('url'),
     path = require('path');
 
+var staticResourceExtns = ['.html','.js','.css','.jpg','.png']
+String.prototype.endsWith = function(extn){
+    return this.substr(this.length - extn.length) === extn;
+}
+
+function isStatic(resourceName){
+    return staticResourceExtns.some(function(extn){
+        return resourceName.endsWith(extn);
+    });
+}
+
 function onConnectionHandle(req,res){
-    console.log(url.parse(req.url,true));
-    var pathname = url.parse(req.url).pathname;
+    var urlObj = url.parse(req.url,true);
+    var pathname = urlObj.pathname;
     if (pathname === "/")
         pathname = "/index.html";
-    var fileName = path.join(__dirname,pathname );
-    fs.stat(fileName, function(error, stats){
-        if (error){
-            res.statusCode = 500;
+    if (isStatic(pathname)){
+        var fileName = path.join(__dirname,pathname );
+        var fileFound = fs.existsSync(fileName);
+        if (fileFound){
+            var rs = fs.createReadStream(fileName,{encoding : 'utf-8'});
+            rs.pipe(res);
+        } else {
+            res.statusCode = 404;
             res.end();
         }
-        if (stats.isFile()){
-            var fileFound = fs.existsSync(fileName);
-            if (fileFound){
-                var rs = fs.createReadStream(fileName,{encoding : 'utf-8'});
-                rs.pipe(res);
-            } else {
-                res.statusCode = 404;
-                res.end();
-            }
-        } else {
-            if (pathname === "/calculator"){
-
-            }
-        }
-    });
-
-
+    } else if (pathname === "/calculator") {
+        res.write("calculator is being processed for " + JSON.stringify(urlObj.query));
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 }
 var server = http.createServer(onConnectionHandle);
 server.listen("8080");
